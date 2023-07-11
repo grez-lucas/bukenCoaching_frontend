a1c88b06c8fa659bec3645a93589886d0d0d48dc
grez-lucas <lgrez1@miuandes.cl>
2023-07-11 13:57:07 -0400

    feat: fourth slide of home view

--- buken-coaching/src/components/Home.tsx
+++ buken-coaching/src/components/Home.tsx
@@ -133,7 +133,8 @@ function Home() {
           </li>
         </ul>
         <p className="text-start mb-0 p-0">
-        ¿Quieres descubrir cómo mi coaching individualizado puede impulsar tu éxito y llevar tus resultados al siguiente nivel?
+          ¿Quieres descubrir cómo mi coaching individualizado puede impulsar tu
+          éxito y llevar tus resultados al siguiente nivel?
         </p>
         <p className="text-start mb-2">
           Aplica para coaching 1:1 rellenando un breve formulario para comenzar!
@@ -148,7 +149,141 @@ function Home() {
           <button className="bg-gradient-to-t from-success to-success-500 rounded-lg p-5 font-bold text-white">
             Aplica para Coaching 1:1
           </button>
-          <button className="bg-white border-[1px] border-black text-black rounded-lg p-5">Agenda una Consulta Gratis</button>
+          <button className="bg-white border-[1px] border-black text-black rounded-lg p-5">
+            Agenda una Consulta Gratis
+          </button>
+        </div>
+      </div>
+      {/* Fourth Slide */}
+      <div className="bg-[#227447] h-[300vh] w-full grid gap-0 text-white px-4 pt-8 pb-4">
+        <div className="bg-white h-[339px] rounded-md"></div>
+        <h2 className="text-center text-3xl font-bold my-4">Servicios</h2>
+        <div className="flex flex-col justify-center gap-2">
+          <p className="text-start">
+            Mi enfoque de coaching se basa en la personalización y la
+            transparencia, para que puedas seguir de cerca los ajustes semana a
+            semana y adquirir conocimientos valiosos para alcanzar tus metas.
+          </p>
+          <p className="text-start">
+            Adicionalmente, mi estilo de comunicacion y guianza es 24/7, con
+            respuestas usualmente en menos de una hora. La puerta siempre esta
+            abierta para dudas o inquietudes a lo largo del camino hacia tus
+            metas.
+          </p>
+          <div className="bg-white rounded-md p-5 my-1 text-success-600">
+            <h3 className="text-center text-2xl font-bold mb-4 underline">
+              Coaching de Dieta
+            </h3>
+            <ul className="list-disc text-base ml-2 ">
+              <li className="mb-3">
+                <p>Plan nutricional basado en tus objetivos y preferencias</p>
+                <li>
+                  Basado en Macros o plan de comidas, con objetivos de
+                  NEAT/cardio
+                </li>
+              </li>
+              <li className="mb-3">
+                <p>
+                  Check-ins semanales con ajustes basados en tu progreso y
+                  feedback
+                </p>
+              </li>
+              <li className="mb-3">
+                <p>Comunicación 24/7 para dudas o inquietudes</p>
+              </li>
+              <li className="mb-3">
+                <p>
+                  Acceso a plantillas de dieta y progreso semanal de peso
+                  corporal
+                </p>
+              </li>
+              <li className="mb-3">
+                <p>Compromiso </p>
+              </li>
+            </ul>
+          </div>
+          <div className="bg-white rounded-md p-5 my-1 text-success-600">
+            <h3 className="text-center text-2xl font-bold mb-4 underline">
+              Coaching de Entrenamiento
+            </h3>
+            <ul className="list-disc text-base ml-2">
+              <li className="mb-3">
+                <p>
+                  Programa de entrenamiento de hipertrofia construido
+                  especificamente para ti y tus metas
+                </p>
+              </li>
+              <li className="mb-3 ">
+                <p>
+                  Check-ins semanales con ajustes basados en tu progreso y
+                  feedback
+                </p>
+              </li>
+              <li className="mb-3">
+                <p>Comunicación 24/7 para dudas o inquietudes</p>
+              </li>
+              <li className="mb-3">
+                <p>Acceso a plantillas de entrenamiento</p>
+              </li>
+              <li className="mb-3">
+                <p>Retoalimentación de técnica en ejercicios </p>
+              </li>
+              <li className="mb-3">
+                <p>Compromiso </p>
+              </li>
+            </ul>
+          </div>
+          <div className="bg-white rounded-md p-5 my-1 text-success-600">
+            <h3 className="text-center text-2xl font-bold mb-4 underline">
+              Coaching de Dieta + Entrenamiento
+            </h3>
+            <ul className="list-disc text-base ml-2">
+              <li className="mb-3">
+                <p className="underline italic">
+                  Recomendado para mejores resultados 
+                </p>
+              </li>
+              <li className="mb-3">
+                <p>Plan nutricional basado en tus objetivos y preferencias</p>
+                <li className="list-[square] ml-4">
+                  Basado en Macros o plan de comidas, con objetivos de
+                  NEAT/cardio
+                </li>
+              </li>
+              <li className="mb-3">
+                <p>
+                  Programa de entrenamiento de hipertrofia construido
+                  especificamente para ti y tus metas
+                </p>
+              </li>
+              <li className="mb-3 ">
+                <p>
+                  Check-ins semanales con ajustes basados en tu progreso y
+                  feedback
+                </p>
+              </li>
+              <li className="mb-3">
+                <p>Comunicación 24/7 para dudas o inquietudes</p>
+              </li>
+              <li className="mb-3">
+                <p>Acceso a plantillas de entrenamiento, dieta y progreso semanal de peso corporal</p>
+              </li>
+              <li className="mb-3">
+                <p>Retoalimentación de técnica en ejercicios </p>
+              </li>
+              <li className="mb-3">
+                <p>Compromiso </p>
+              </li>
+            </ul>
+          </div>
+          <div className="mt-4 flex flex-col gap-2">
+            <button className="bg-gradient-to-t from-success to-success-500 rounded-lg p-5 font-bold text-white">
+              Aplica para Coaching 1:1
+            </button>
+            <button className="bg-transparent border-[1px] border-white text-white rounded-lg p-5">
+              Agenda una Consulta Gratis
+            </button>
+          </div>
         </div>
       </div>
     </>
