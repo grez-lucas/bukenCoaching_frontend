89142ee41837804ae34df541c95158b73c663d33
grez-lucas <lgrez1@miuandes.cl>
2023-07-17 13:01:58 -0400

    fix: z-index on header

--- buken-coaching/src/components/Header.tsx
+++ buken-coaching/src/components/Header.tsx
@@ -24,7 +24,7 @@ function Header( {setSelectedOptionMain}: HeaderProps ) {
 
   return (
     <>
-    <div className="fixed top-0 left-0 right-0 grid grid-cols-12 grid-rows-1 content-center bg-success h-[10vh]">
+    <div className="fixed top-0 left-0 right-0 z-40 grid grid-cols-12 grid-rows-1 content-center bg-success h-[10vh]">
       <img
         src={LogoRectangle}
         alt="Buken Coaching Logo"
