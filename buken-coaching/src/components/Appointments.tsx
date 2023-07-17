c9a79d754bc911bf079d5ac654ffeed500f3e233
grez-lucas <lgrez1@miuandes.cl>
2023-07-17 19:13:06 -0400

    refactor: ESLINT warning for title on iframe

--- buken-coaching/src/components/Appointments.tsx
+++ buken-coaching/src/components/Appointments.tsx
@@ -3,6 +3,7 @@ function Appointments() {
     <div className="mt-[10vh] bg-white text-slate-200 divide-y-4 divide-main">
       <section className=" flex flex-col items-center">
           <iframe
+          title="Appointments"
             src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ0URxqmicDMS4kgGh-OjaQ_5_OfcudzaJt38bxfLL8mYDPwypStXyfVfGIuv2FAMkUWpRVjGezV?gv=true"
             // style="border: 0"
             width="100%"
