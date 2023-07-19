2cd9b58f4c3edf96d9b78357e68164019f8f9307
grez-lucas <lgrez1@miuandes.cl>
2023-07-18 20:41:55 -0400

    fix: linked handler to element

--- buken-coaching/src/components/Footer.tsx
+++ buken-coaching/src/components/Footer.tsx
@@ -52,7 +52,7 @@ function Footer() {
           </div>
 
           {/* INSTA ICON */}
-          <div className="cursor-pointer">
+          <div className="cursor-pointer" onClick={handleInstagramClick}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
